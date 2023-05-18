61dae634b297238dd16ff880f315c4c981399a1c
khasanokhtov <76727992+khasanokhtov@users.noreply.github.com>
2023-05-18 10:00:22 +0300

    Initial Commit

---
+++ CryptoCurrMVVM/CryptoCurrMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrMVVMApp.swift
+//  CryptoCurrMVVM
+//
+//  Created by Alex Okhtov on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
